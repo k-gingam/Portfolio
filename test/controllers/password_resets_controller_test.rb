1863612296aa23de29a2982cb8a70cb05d296f8c
root <root@DESKTOP-KAC6OHH>
2025-02-09 22:15:34 +0900

    パスワードリセット機能の実装

--- test/controllers/password_resets_controller_test.rb
+++ test/controllers/password_resets_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create(name: "User", email: "test@example.com", password: "password") # テスト用ユーザーを作成
+  end
+
   test "should get create" do
     get password_resets_create_url
     assert_response :success
