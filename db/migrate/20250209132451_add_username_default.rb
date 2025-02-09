c9741e02d3e7bf83c8a82979155b3756304b9897
root <root@DESKTOP-KAC6OHH>
2025-02-09 22:40:37 +0900

    パスワードリセット機能の実装

--- db/migrate/20250209132451_add_username_default.rb
+++ db/migrate/20250209132451_add_username_default.rb
@@ -1,6 +1,7 @@
 class AddUsernameDefault < ActiveRecord::Migration[7.2]
   def change
     # テストコード実行の際、User.nameがnilの時の対策としてデフォルト値を設定
-    change_column_default :users, :name, "User"
+    change_column_default :users, :name, from: nil, to: "Name"
+    change_column_default :users, :email, from: nil, to: "test@example.com"
   end
 end
--- test/controllers/password_resets_controller_test.rb
+++ test/controllers/password_resets_controller_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
-  
   test "should get create" do
     get password_resets_create_url
     assert_response :success
