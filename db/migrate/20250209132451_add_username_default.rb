0076436ad7bfc984760e9f07be96911ca982aaeb
root <root@DESKTOP-KAC6OHH>
2025-02-09 22:47:27 +0900

    パスワードリセット機能の実装

--- db/migrate/20250209132451_add_username_default.rb
+++
@@ -1,7 +0,0 @@
-class AddUsernameDefault < ActiveRecord::Migration[7.2]
-  def change
-    # テストコード実行の際、User.nameがnilの時の対策としてデフォルト値を設定
-    change_column_default :users, :name, from: nil, to: "Name"
-    change_column_default :users, :email, from: nil, to: "test@example.com"
-  end
-end
